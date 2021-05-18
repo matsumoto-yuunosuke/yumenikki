6871bb90eefc9e9a20f32034ab95023a9fa3b2cf
マツモト・ユウノスケ <matsumotoyuunosuke@matsumotoyuunosukenoMacBook-Air.local>
2021-05-18 14:59:44 +0900

    #5 update idea_delate

--- yume/yumenikki/urls.py
+++ yume/yumenikki/urls.py
@@ -5,11 +5,12 @@ from . import views
 urlpatterns = [
   path('dream_list/', views.DreamList.as_view(), name='dream_list'),
   path('dream_detail/<int:pk>/', views.DreamDetail.as_view(), name='dream_detail'),
-  path('dream_idea_detail/<int:pk>/', views.DreamIdeaDetail.as_view(), name='dream_idea_detail'),
   path('dream_update/<int:pk>/', views.DreamUpdate.as_view(), name='dream_update'),
   path('dream_delete/<int:pk>/', views.DreamDelete.as_view(), name='dream_delete'),
   path('dream_create/', views.dream_upload, name='dream_create'),
   path('idea_list/', views.IdeaList.as_view(), name='idea_list'),
+  path('dream_idea_detail/<int:pk>/', views.DreamIdeaDetail.as_view(), name='dream_idea_detail'),
   path('idea_detail/<int:pk>/', views.IdeaDetail.as_view(), name='idea_detail'),
   path('idea_create/', views.idea_upload, name='idea_create'),
+  path('idea_delete/<int:pk>/', views.IdeaDelete.as_view(), name='idea_delete'),
 ]
