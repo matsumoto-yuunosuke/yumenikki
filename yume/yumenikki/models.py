51c7fd98d1f626951a0a7aee8de2866957a821e1
マツモト・ユウノスケ <matsumotoyuunosuke@matsumotoyuunosukenoMacBook-Air.local>
2021-06-14 22:20:09 +0900

    #30

--- yume/yumenikki/models.py
+++ yume/yumenikki/models.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
         if not username:
             raise ValueError('Users must have an username address')
         user = self.model(
-            username = self.normalize_username(username)
+            username = self.model.normalize_username(username)
         )
         user.set_password(password)
         user.save(using=self._db)
